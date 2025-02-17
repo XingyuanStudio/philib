5c7a22b8a673a1b55e31f554723a3f3c834a5333
machenxiu <mcx20110507@outlook.com>
2025-02-17 15:22:35 +0800

    feat: calc_best_n完成

--- calc_best_n.py
+++ calc_best_n.py
@@ -1,27 +1,67 @@
-def calc_best_n(self, phi_n: int = 3, best_n: int = 27) -> dict:  # TODO @machenxiu
-        """
-        {
-            "phi_list":[
-                {
-                    "song_name": 曲目名称,
-                    "level": 谱面等级 [ez|hd|in|at],
-                    "rks": 谱面rks
-                    "difficulty": 谱面定数
-                    "acc": 准确度
-                }
-                ...
-            ],
-            "best_list":[
-                {
-                    "song_name": 曲目名称,
-                    "level": 谱面等级 [ez|hd|in|at],
-                    "rks": 谱面rks
-                    "difficulty": 谱面定数
-                    "acc": 准确度
-                }
-                ...
-            ]
-        }
-        """
+from calc_chart_score import *
 
- 
+
+def calc_best_n(self, phi_n, best_n):
+    """
+    {
+        "phi_list":[
+            {
+                "song_name": 曲目名称,
+                "level": 谱面等级 [ez|hd|in|at],
+                "rks": 谱面rks
+                "difficulty": 谱面定数
+                "acc": 准确度
+            }
+            ...
+        ],
+        "best_list":[
+            {
+                "song_name": 曲目名称,
+                "level": 谱面等级 [ez|hd|in|at],
+                "rks": 谱面rks
+                "level": 谱面定数
+                "acc": 准确度
+            }
+            ...
+        ]
+    }
+    """
+    level_list = ["ez", "hd", "in", "at"]
+    all_list = []
+    phi_list = []
+    best_list = []
+    for every in self.game_record.items():
+        for level in level_list:
+            if every[1][level]["score"] == 1000000:
+                phi_list.append(
+                    {
+                        "sone_name": every[0],
+                        "level": level,
+                        "level": self.chart_level_data[every[0]][level],
+                        "acc": every[1][level]["acc"],
+                        "rks": calc_chart_score(
+                            every[1][level]["acc"],
+                            self.chart_level_data[every[0]][level],
+                        ),
+                    }
+                )
+            all_list.append(
+                {
+                    "sone_name": every[0],
+                    "level": level,
+                    "level": self.chart_level_data[every[0]][level],
+                    "acc": every[1][level]["acc"],
+                    "rks": calc_chart_score(
+                        every[1][level]["acc"],
+                        self.chart_level_data[every[0]][level],
+                    ),
+                }
+            )
+
+    phi_list.sort(key=lambda x: x["rks"], reverse=True)
+    phi_list = phi_list[:phi_n]
+    best_list = all_list[:best_n]
+    return {
+        "phi_list": phi_list,
+        "best_list": best_list,
+    }
--- main.py
+++ main.py
@@ -38,13 +38,14 @@ else:
 phigros.load_level_data = phigros.load_difficulty  # 重命名函数
 phigros.load_level_data(bytes("Library/level_data.tsv", "utf-8"))
 
+
 class PhigrosGet:
-    def __init__(self, sessionToken: str|bytes):
+    def __init__(self, sessionToken: str | bytes):
         if isinstance(sessionToken, str):  # 如果sessionToken是字符串，则将其转换为bytes
             self.sessionToken = bytes(sessionToken, "utf-8")  # 使用utf-8编码
         else:
             self.sessionToken = sessionToken  # 如果sessionToken已经是bytes，则直接使用
-            
+
         self.handle = phigros.get_handle(self.sessionToken)  # 获取handle
         if not self.handle:  # 如果handle获取失败，则抛出异常
             raise RuntimeError("Failed to get handle")
@@ -53,11 +54,11 @@ class PhigrosGet:
         self.b19 = json.loads(phigros.get_b19(self.handle).decode("utf-8"))
         self.summary = json.loads(phigros.get_summary(self.handle).decode("utf-8"))
         self.save = json.loads(phigros.get_save(self.handle).decode("utf-8"))
-        
+
         # 加载数据
         self.game_record = None
         self.get_game_record()
-        
+
         with open("level_data.json", "r", encoding="utf-8") as f:
             self.chart_level_data = json.load(f)
 
@@ -75,7 +76,7 @@ class PhigrosGet:
 
     def get_game_record(self) -> Dict[str, Dict[str, Dict[str, Union[int, float]]]]:
         """返回经整理过的用户游戏记录数据
-        
+
         Returns:
             Dict[str, List[Dict[str, Union[int, float]]]]: 游戏记录
             - 键: 曲目名称 (str)
@@ -84,7 +85,7 @@ class PhigrosGet:
                     - score: 分数 (int)
                     - acc: 准确度 (float)
                     - fc: 是否FC (int)
-                    
+
         Example:
         {
             "song_name": {
@@ -102,33 +103,36 @@ class PhigrosGet:
                 "at": {
                     ...
                 },
-                
+
             }
         }
         """
         if self.game_record:  # 如果游戏记录已经存在，则直接返回，避免重复计算
             return self.game_record
-        
+
         game_record: Dict[str, Dict[str, Dict[str, Union[int, float]]]] = {}
-        
+
         for song_name, records in self.save["gameRecord"].items():
             game_record[song_name] = {}
             # 每3个元素是一个难度的记录（分数、准确度、是否FC），包裹在字典中
-            for i in range(0, len(records), 3):  
-                score, acc, fc = records[i:i+3]
+            for i in range(0, len(records), 3):
+                score, acc, fc = records[i : i + 3]
                 difficulty_map = {0: "ez", 3: "hd", 6: "in", 9: "at"}
                 difficulty = difficulty_map[i]
-                game_record[song_name][difficulty] = {  
+                game_record[song_name][difficulty] = {
                     "score": score,
                     "acc": acc,
-                    "fc": fc
+                    "fc": fc,
                 }
         self.game_record = game_record
-        
+
     def get_rks_suggest(
         self, rks_wanted: float = 0.01
     ) -> dict: ...  # TODO @Xingyuan55  See #2
-    
+
+    def calc_best_n(self, phi_n: int = 3, best_n: int = 27) -> dict:
+        return calc_best_n(self, phi_n, best_n)
+
     def __del__(self):
         if hasattr(self, "handle") and self.handle:
             try:
@@ -140,8 +144,9 @@ class PhigrosGet:
 # Example usage
 if __name__ == "__main__":
     user = PhigrosGet("ztl8rh36krtgro724jo83f3o5")
-    print(user.game_record)
+    # print(user.game_record)
     # with open("b191.json", "w", encoding="utf-8") as f:
     #     json.dump(user.b19, f, indent=4, ensure_ascii=False)
-    with open("game_record2.json", "w", encoding="utf-8") as f:
-        json.dump(user.game_record, f, indent=4, ensure_ascii=False)
+    # with open("game_record2.json", "w", encoding="utf-8") as f:
+    #     json.dump(user.game_record, f, indent=4, ensure_ascii=False)
+    print(user.calc_best_n())
